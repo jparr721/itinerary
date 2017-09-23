40b9afbaa4dff57bbf1d2d80ccc1e6f09423e3be
Jarred Parr <jparr721@gmail.com>
2017-09-23 19:22:34 -0400

    merging

--- itinerary/src/components/ModalSearch.js
+++ itinerary/src/components/ModalSearch.js
@@ -4,24 +4,32 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 
 class ModalSearch extends Component {
+  handleChange = location => event => {
+    this.setState({
+      [location]: event.target.value,
+    });
+  };
   render() {
     return (
       <div className="modal-search">
         <h1>Where would you like to go?</h1>
         <form noValidate autoComplete="off">
-          <TextField
-              id="full-width"
-              label="Search!"
-              InputProps={{ placeholder: 'Paris, France' }}
-              helperText="Vacation in your dream spot"
-              fullWidth
-              margin="normal"
-            />
+          <h3>
+            <TextField
+                id="full-width"
+                label="Search!"
+                InputProps={{ placeholder: 'Paris, France' }}
+                helperText="Vacation in your dream spot"
+                onChange={this.handleChange('location')}
+                fullWidth
+                margin="normal"
+              />
+            </h3>
           </form>
-        {/* <iframe className="map"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDpZF4UlkJ1bIvg5sG29oyxWfR20fODMDI
-            &q=Space+Needle,Seattle+WA" allowFullScreen>
-        </iframe> */}
+        <iframe className="map"
+          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDpZF4UlkJ1bIvg5sG29oyxWfR20fODMDI
+            &q=${this.state.location}`} allowFullScreen>
+        </iframe>
       </div>
     );
   };

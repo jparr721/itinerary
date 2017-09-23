07902262667432cdacecfc69d6f668b434690be9
Jarred Parr <jparr721@gmail.com>
2017-09-23 13:54:21 -0400

    More modal stuff too

--- itinerary/src/components/Modal.js
+++ itinerary/src/components/Modal.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+// Pages
+import ModalSearch from './ModalSearch';
+
+// Material UI
 import Button from 'material-ui/Button';
 import AddIcon from 'material-ui-icons/Add';
 import Tooltip from 'material-ui/Tooltip';
 import IconButton from 'material-ui/IconButton';
-import Backspace from 'material-ui-icons/Backspace';
+import NavigateBefore from 'material-ui-icons/NavigateBefore';
+import BottomNavigation, { BottomNavigationButton } from 'material-ui/BottomNavigation';
+import AddLocationIcon from 'material-ui-icons/AddLocation';
 
 class Modal extends Component {
   constructor () {
     super();
     this.state = {
-      showModal: false
+      showModal: false,
+      value: 'whereto',
+      children: ModalSearch,
     };
-
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
   }
+
+  handleChange = (event, value, children) => {
+    this.setState({ value, children });
+  };
 
   handleOpenModal () {
     this.setState({ showModal: true });
@@ -25,7 +36,10 @@ class Modal extends Component {
   handleCloseModal () {
     this.setState({ showModal: false });
   }
+
+
   render() {
+    const { value, children } = this.state;
     return (
       <div>
         <Tooltip placement="top" title="Start your new adventure!">
@@ -36,8 +50,9 @@ class Modal extends Component {
         <ReactModal
           isOpen={this.state.showModal}>
           <IconButton color="primary" aria-label="Add to shopping cart" onClick={this.handleCloseModal}>
-            <Backspace />
+            <NavigateBefore />
           </IconButton>
+
         </ReactModal>
       </div>
     );
---
+++ itinerary/src/components/ModalSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ModalSearch extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Where would you like to go?</h1>
+      </div>
+    );
+  };
+}
+
+export default ModalSearch;

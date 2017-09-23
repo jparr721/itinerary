12ba0255234f57e946e6adcadaad81a5373d5aa3
Jarred Parr <jparr721@gmail.com>
2017-09-23 00:22:36 -0400

    more router bugs

--- itinerary/src/components/Routes.js
+++ itinerary/src/components/Routes.js
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Page imports
 import Home from './Home';
+import Login from './Login';
 
 const Routes = () => {
   return (
     <Router>
+      <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </Router>
   );
 }

259809b33aa2eb44252349216c0ef4c556555fe3
Jarred Parr <jparr721@gmail.com>
2017-09-22 23:44:46 -0400

    About to add material ui

--- itinerary/src/components/Router.js
+++ itinerary/src/components/Router.js
@@ -5,5 +5,11 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 
 const Routes = () => {
-  
+  return (
+    <routes>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </routes>
+  )
 }

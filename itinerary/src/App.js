63bb78e1f29faa098adf6b1858d67fadb88e91c8
John Doneth <doneth7@gmail.com>
2017-09-23 03:01:12 -0400

    I'm tired :sleeping:

--- itinerary/src/App.js
+++ itinerary/src/App.js
@@ -27,8 +27,8 @@ class App extends Component {
     return (
       <MuiThemeProvider>
         <div>
-            <Header />
-            <Routes />
+          <Header />
+          <Routes />
         </div>
       </MuiThemeProvider>
     );

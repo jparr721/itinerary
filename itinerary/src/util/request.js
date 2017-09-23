17e8bef3e8916f095fb9300b27b6045a483fac9f
John Doneth <doneth7@gmail.com>
2017-09-23 17:07:20 -0400

    Magical cloud link established

--- itinerary/src/util/request.js
+++ itinerary/src/util/request.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 class Request {
 
     constructor() {
-        this.baseUrl = "http://localhost:8080/";
+        //this.baseUrl = "http://localhost:8080/";
+        this.baseUrl = "http://travelapi-env.2xnegqgnvd.us-east-2.elasticbeanstalk.com/";
     }
 
     get(url) {

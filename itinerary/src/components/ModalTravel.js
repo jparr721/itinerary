025969d75d59c924d296ed5c6b61603e75310516
Jarred Parr <jparr721@gmail.com>
2017-09-23 21:38:03 -0400

    Finishing view on some of the modal pages

--- itinerary/src/components/ModalTravel.js
+++ itinerary/src/components/ModalTravel.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+
 class ModalTravel extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>How will you embark on your adventure?</h1>
+      <div className="row text-center transport-options">
+          <div className="col-3">
+            <Card className="selection-card">
+              <i className="fa fa-car fa-5x"></i>
+              <h2>Car</h2>
+            </Card>
+          </div>
+          <div className="col-3">
+            <Card className="selection-card">
+              <i className="fa fa-plane fa-5x"></i>
+              <h2>Plane</h2>
+            </Card>
+          </div>
+          <div className="col-3">
+            <Card className="selection-card">
+              <i className="fa fa-train fa-5x"></i>
+              <h2>Train</h2>
+            </Card>
+          </div>
+          <div className="col-3">
+            <Card className="selection-card">
+              <i className="fa fa-bus fa-5x"></i>
+              <h2>Bus</h2>
+            </Card>
+          </div>
+        </div>
       </div>
     );
   };

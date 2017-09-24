32a43683f937c2c701a24126f50ba523bc102db4
Jarred Parr <jparr721@gmail.com>
2017-09-23 20:37:23 -0400

    Reset modal every entry

--- itinerary/src/components/ModalStay.js
+++ itinerary/src/components/ModalStay.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 
+// Material UI
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+
 class ModalStay extends Component {
   render() {
     return (
       <div>
         <h1>Where would you like to stay?</h1>
+        <div className="row">
+          <div className="col-4">
+            <Card className="main-card">
+              <Typography type="title" clasName="title">
+                Hotels Here
+              </Typography>
+              <CardContent>
+                Hotel Info and Price
+              </CardContent>
+            </Card>
+          </div>
+          <div className="col-4">
+            <Card className="main-card">
+              <Typography type="title" clasName="title">
+                Hotels Here
+              </Typography>
+              <CardContent>
+                Hotel Info and Price
+              </CardContent>
+            </Card>
+          </div>
+          <div className="col-4">
+            <Card className="main-card">
+              <Typography type="title" clasName="title">
+                Hotels Here
+              </Typography>
+              <CardContent>
+                Hotel Info and Price
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
     );
   };
